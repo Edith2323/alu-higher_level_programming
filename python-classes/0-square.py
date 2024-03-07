6da35b610c27a5204ca9068119d87008c7813886
Edith2323 <e.githinji1@alustudent.com>
2024-03-07 05:24:46 -0800

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-        Square = __import__('0-square').Square
+        """Square class to represent a square"""
 
-        my_square = Square()
-        print(type(my_square))
-        print(my_square.__dict__)
+
+        class Square:
+            """
+                Defines a Square and its basic properties
+                    """
+                        pass
