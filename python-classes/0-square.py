fd59498595a91340794347c45be3df835c79763a
Edith2323 <e.githinji1@alustudent.com>
2024-03-07 05:20:42 -0800

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Create an Empty Square """
+        Square = __import__('0-square').Square
 
-class Square:
-    ''' creates an empty square'''
-    pass
+        my_square = Square()
+        print(type(my_square))
+        print(my_square.__dict__)
