3ec1902e23a50dc86e468b680a53a406dbf82551
Edith2323 <e.githinji1@alustudent.com>
2024-04-13 11:45:26 -0700

    model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
-"""prints all City objects from the database hbtn_0e_14_usa"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+"""
+contains the class City
+"""
+
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base, State
-from model_city import City
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
-    session = Session(engine)
-
-    states = session.query(State, City) \
-        .filter(State.id == City.state_id).order_by(City.id).all()
-
-    for state in states:
-        print("{}: ({}) {}".format(
-            state[0].__dict__['name'],
-            state[1].__dict__['id'],
-            state[1].__dict__['name']))
-
-    session.close()
+class City(Base):
+    """Representation of a city"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
